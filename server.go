f7bb48ba5936b76d8957f6ca55330352cf0b013f
sumana2001 <sumana.basu2001@gmail.com>
2022-02-17 23:27:58 +0530

    Database

--- server.go
+++ server.go
@@ -4,11 +4,12 @@ import (
 	"log"
 	"net/http"
 	"os"
-
+	database "github.com/sumana2001/hackernews/internal/pkg/db/mysql"
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/sumana2001/hackernews/graph"
 	"github.com/sumana2001/hackernews/graph/generated"
+	"github.com/go-chi/chi"
 )
 
 const defaultPort = "8080"
@@ -19,11 +20,14 @@ func main() {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+	router := chi.NewRouter()
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	database.InitDB()
+	database.Migrate()
+	server := handler.NewDefaultServer(hackernews.NewExecutableSchema(hackernews.Config{Resolvers: &hackernews.Resolver{}}))
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", server)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
